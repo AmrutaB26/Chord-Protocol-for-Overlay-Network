6cd15eb26564624f0ddde872fb29fedf71b7a8d5
Shreya <shreya171995@gmail.com>
2018-10-16 18:40:43 -0400

    Network creation

--- lib/chord.ex
+++ lib/chord.ex
@@ -1,33 +1,95 @@
 defmodule CHORD do
-  @list1 []
   use GenServer
 
   ## ------------------------ Callback functions ----------------------- ##
 
   def start_link(num) do
-    #@list1 ["sjdhk"]
-    IO.inspect @list1
     nodeName = "Node_" <> Integer.to_string(num)
-    hashName = :crypto.hash(:sha, "Node_" <> Integer.to_string(num))
-    GenServer.start_link(__MODULE__,[hashName,[]], name: String.to_atom(nodeName))
+    hashName = :crypto.hash(:sha, "Node_" <> Integer.to_string(num)) |> Base.encode16
+    :ets.insert(:table, {nodeName, hashName}) # -------------------------- try to change
+    GenServer.start_link(__MODULE__,[hashName,%{}], name: String.to_atom(nodeName))
   end
 
   def init(state) do
-    IO.inspect state
     Process.flag(:trap_exit, true)
     {:ok, state}
+  end
+
+  def handle_call({:fingerTable, val}, _from, state) do
+    [hash,_] = Enum.at(state,1)
+    state = [hash,val]
+    {:reply, state,state}
   end
 
   def handle_call({:update,hashKey},_from,state) do
     [hashName,key] = state
     state = [hashName,key | hashKey]
+    IO.inspect state
     {:reply, state,state}
   end
 
-  def fingerTable do
+  # ---------------------- Network Creation ------------------------ ##
 
+  def createNetwork(numNodes) do
+    hashList = sortHash(numNodes)
+    m = :math.log2(numNodes) |> Float.floor |> round
+    createFingerTables(m, hashList)
   end
-  def lookup do
 
+  def sortHash(numNodes) do
+    Enum.map(1..numNodes, fn x ->
+      [{_,list}] = :ets.lookup(:table,"Node_" <> Integer.to_string(x))
+      list
+    end)
+    |> Enum.sort()
+  end
+
+  def createFingerTables(m, hashList) do
+    Enum.map(hashList, fn x->
+      index = Enum.find_index(hashList, fn y -> y == x end)
+      map = %{}
+      map = fingerTable(0, map, index, m, hashList)
+      GenServer.call(String.to_atom("Node_" <> Integer.to_string(index+1)), {:fingerTable, map})
+    end)
+  end
+
+  def fingerTable(i, map, index, m, hashList) do
+      if(i == m+1) do
+        map
+      else
+        start = rem(index + round(:math.pow(2,i)), round(:math.pow(2,m)))
+        successor = if(i < m) do
+          Enum.at(hashList, index+1)   ## ------------------- if to be stored based on index????
+        else
+          Enum.at(hashList, 0)
+        end
+        map = Map.put(map, start, successor)
+        fingerTable(i+1, map, index, m, hashList)
+      end
+  end
+
+  def getSuccessorNode(key,nodeList) do
+    node =  Enum.find(nodeList, fn x -> :crypto.hash(:sha, x) > key end)
+    if(node == nil) do
+      Enum.at(nodeList,0)
+    else
+     node
+    end
+   end
+
+   def randomString(length) do
+     if(length != 0) do
+       Enum.random(['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'])
+     end
+   end
+
+   def stringGenerator(numRequests,nodeList) do
+    Enum.map(1..2*numRequests, fn x->
+      value = randomString(12)
+      key = :crypto.hash(:sha, value)
+      node = getSuccessorNode(key,nodeList)
+      #storeKeyinNode(node,key)
+      #@valueList = [@valueList | value]
+    end)
   end
 end
--- main.exs
+++ main.exs
@@ -1,3 +1,5 @@
 defmodule MAIN do
+  :ets.new(:table, [:named_table,:public])
   SSUPERVISOR.start_link(10)
+  CHORD.createNetwork(10);
 end
